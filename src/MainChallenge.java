e4a38ae3db558ef30beffc61bd73f21b1a9b9458
Sylvester Praise <sylvesterpraise123456@gmail.com>
2026-03-19 21:50:01 +0100

    updates

--- src/MainChallenge.java
+++ src/MainChallenge.java
@@ -1,29 +1,55 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class MainChallenge {
-    public static void main(String[] args) {
-        boolean gameOver = true;
-        int score = 800;
-        int levelCompleted = 5;
-        int bonus = 100;
+    public static void displayHighScorePosition(String playersName, int position){
+        System.out.println(playersName + "managed to get into position " + position + " on the high score list");
+    }
+
+    public static int calculateHighScorePosition(int playersScore){
+        if (playersScore >= 1000){
+            return  1;
+        }
+        else if (playersScore >= 500 && playersScore < 1000){
+            return 2;
+        }
+        else if (playersScore >= 100 && playersScore < 500){
+            return 3;
+        }
+        else {
+            return 4;
+
+        }
+    }
+
+    public static int  calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
         int finalScore = score;
 
         if(gameOver){
             finalScore += (levelCompleted * bonus);
-            System.out.println("Your final Score is, " + finalScore);
+
         }
+        return finalScore;
 
-        score = 10000;
+    }
+    public static void main(String[] args) {
 
-        boolean gameOver1 = true;
-        int score1 = 10000;
-        int levelCompleted1 = 8;
-        int bonus1= 200;
-        int finalScore1 = score;
 
-        if(gameOver1){
-            finalScore += (levelCompleted1 * bonus);
-            System.out.println("Your final Score is, " + finalScore1);
-        }
+//        System.out.print("Final score is: " + calculateScore(true, 800, 5, 100));
+//
+//        calculateScore(true, 10000, 8,200);
+        int highScorePosition = calculateHighScorePosition(1500);
+        displayHighScorePosition("Praise", highScorePosition);
+        highScorePosition = calculateHighScorePosition(1000);
+        displayHighScorePosition("Favour", highScorePosition);
+        highScorePosition = calculateHighScorePosition(500);
+        displayHighScorePosition("lisa", highScorePosition);
+        highScorePosition = calculateHighScorePosition(100);
+        displayHighScorePosition("Faith", highScorePosition);
+        highScorePosition = calculateHighScorePosition(25);
+        displayHighScorePosition("Bob", highScorePosition);
+
+
+
+
     }
 }
