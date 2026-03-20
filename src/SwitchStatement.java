b2baad8685afa85de1612ef34095d65255de69de
Sylvester Praise <sylvesterpraise123456@gmail.com>
2026-03-21 00:24:29 +0100

    updates

--- src/SwitchStatement.java
+++ src/SwitchStatement.java
@@ -1,2 +1,34 @@
 public class SwitchStatement {
+    public static void main(String[] args) {
+        int switchValue = 2;
+        switch (switchValue) {
+            case 1 -> System.out.println("The Value is 1");
+            case 2 -> System.out.println("The value is 2");
+            case 3 -> System.out.println("The value is 3");
+            default -> System.out.println("Was not any of the cases");
+        }
+
+        char alphabet = 'A';
+        switch(alphabet){
+            case 'A':
+                System.out.println("Able");
+                break;
+            case 'B':
+                System.out.println("Baker");
+                break;
+            case 'C':
+                System.out.println("Charlie");
+                break;
+            case 'D':
+                System.out.println("Dog");
+                break;
+            case 'E':
+                System.out.println("Easy");
+                break;
+            default:
+                System.out.println("Char not in range");
+                break;
+        }
+
+    }
 }
