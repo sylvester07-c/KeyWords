e454be6a0963d2066fb9acf7937e522a715a66d5
Sylvester Praise <sylvesterpraise123456@gmail.com>
2026-03-20 14:30:45 +0100

    updates

--- src/SecondAndMinutesChallenge.java
+++ src/SecondAndMinutesChallenge.java
@@ -1,27 +1,19 @@
 public class SecondAndMinutesChallenge {
     public static String getDurationString(int seconds){
-        int hours = seconds /3600;
-        if (seconds % 3600 == 0) {
-            return hours + "h" + "0m 0s";
+        if(seconds < 0){
+            return  "Invalid input";
         }
-        else if(seconds % 3600 >= 60){
-            int mins = (seconds % 3600) / 60;
-            int secs = seconds % 3600 - 60;
-            return hours + "h " + mins + "m " + secs + "s";
-        }
-        else {
-            return hours + "h " + "0m " + seconds % 3600 + "s";
-        }
-
+        int minutes = seconds / 60;
+        String durationString = getDurationString(minutes, seconds % 60);
+        return durationString;
     }
     public static String getDurationString(int minutes, int seconds) {
         if (minutes >= 0 && seconds >= 0 && seconds <= 59) {
             int hours = minutes / 60;
-            if (minutes % 60 == 0) {
-                return hours + "h" + "0m 0s";
-            } else {
-                return hours + "h " + minutes % 60 + "m " + seconds + "s";
-            }
+            int remainingMinutes = minutes % 60;
+
+            return  hours + "h " + remainingMinutes + "m " + seconds + "s";
+
         } else {
             return "Invalid Inputs";
         }
@@ -29,8 +21,8 @@ public class SecondAndMinutesChallenge {
 
 
     public static void main(String[] args) {
-        System.out.println(getDurationString(3675));
-        System.out.println(getDurationString(450, 10));
+        System.out.println(getDurationString(-4945));
+        System.out.println(getDurationString(65, 60));
 
 
     }
---
+++ src/SwitchStatement.java
@@ -0,0 +1,2 @@
+public class SwitchStatement {
+}
